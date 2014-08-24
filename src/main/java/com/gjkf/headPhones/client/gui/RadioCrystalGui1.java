e550612b89253fb98fda4187dcc8921aff71ff1a
gjkf <cossu.dvd@gmail.com>
2014-08-24 16:45:56 +0200

    The "old" new reference class for coords and stuff

--- src/main/java/com/gjkf/headPhones/client/gui/RadioCrystalGui1.java
+++ src/main/java/com/gjkf/headPhones/client/gui/RadioCrystalGui1.java
@@ -1,114 +1,96 @@
 package com.gjkf.headPhones.client.gui;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import org.lwjgl.input.Keyboard;
+import net.minecraft.entity.player.EntityPlayer;
 
-import com.gjkf.headPhones.connection.Link;
-import com.gjkf.headPhones.items.RadioCrystalContainer;
+import com.gjkf.headPhones.Main;
 import com.gjkf.lib.gui.GuiGJButton;
 import com.gjkf.lib.gui.GuiGJTextField;
+import com.gjkf.lib.gui.GuiScreenWidget;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.GuiTextField;
-import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
+public class RadioCrystalGui1 extends GuiScreenWidget{
 
-public class RadioCrystalGui1 extends GuiScreen{
-
-	private final int ID_PAGE_LEFT = 1;
-	private final int ID_PAGE_RIGHT = 2;
-	private final int ID_CONNECT = 3;
-	private final int ID_CONFIRM = 4;
-	private final int ID_ADD = 5;
-	private final int ID_CANCEL = 6;
-	private final int ID_RENAME = 7;
-	private final int ID_DELETE = 8;
-	private final int ID_FAVOURITE = 9;
-	private final int ID_SEARCH = 10;
-	private final int ID_NONE = 11;
-	private final int ID_CATEGORIES = 12;
-	private final int ID_CLOSE = 13;
-	private final int ID_FAVOURITES = 14;
-
-	private GuiGJTextField textField;
 	private GuiGJTextField insertField;
+	private GuiGJTextField listField;
+
+	private int pageNumber = 0;
 	
-	public String textFieldText = "";
-	public String insertFieldText = "";
+	public int midX;
+	public int midY;
+	
+	private String allowedChars = "abcdefghijklmnopqrstuvwxyz!/()=?.";
+	public String insertFieldText;
+	public ArrayList<String> listFieldText;
+	public ArrayList<String> favList;
+	
+	public char[] allowedChar = new char[33];
 
-	private boolean confirmed = false;
-	private boolean adding = false;
-	private boolean renaming = false;
-	private boolean deleting = false;
-	private boolean hasClicked = false;
-
-	private boolean enabledTextField = false;
-
-	private static final String[] invalidChars = new String[] { "\\", "*", "?", "\"", "<", ">", "|", "!"};
-
-	public List<String> availableLinks;
-	public List<String> linksToShow;
-	public List<String> categories;
-	public List<String> categoryLink = new ArrayList<String>();
-	public List<String> enabledButtons = new ArrayList<String>();
-
-	public EntityPlayer player;
-	public Link link;
-
-	protected int xSize = 256;
-	protected int ySize = 100;
-
-	protected int guiLeft;
-	protected int guiTop;
-
-	public int pageNumber = 0;
-
-	public RadioCrystalGui1(Container inv, EntityPlayer ply){
+	public RadioCrystalGui1(EntityPlayer ply){
 		super();
-		this.player = ply;
 	}
 
-	public void initGui(){
-		super.initGui();
-
-		if(player == null){
-			mc.displayGuiScreen(null);
-		}else{
-			Keyboard.enableRepeatEvents(true);
-			buttonList.clear();
-
-			this.guiLeft = (this.width - this.xSize) / 2;
-			this.guiTop = (this.height - this.ySize) / 2;
-
-			buttonList.add(new GuiGJButton(width / 2 + 47, height / 2 + 54, 20, 20, ">"));
-			buttonList.add(new GuiGJButton(width / 2 - 21, height / 2 + 54, 20, 20, "<"));
-			buttonList.add(new GuiGJButton(width / 2 - 91, height / 2 + 54, 44, 20,"Connect"));
-			buttonList.add(new GuiGJButton(width / 2 + 1, height / 2 + 54, 44, 20, "Add"));
-			buttonList.add(new GuiGJButton(width / 2 + 1, height / 2 + 24, 44, 20, "Delete"));
-			buttonList.add(new GuiGJButton(width / 2 - 95, height / 2 + 24, 52, 20, "Favourite"));
-			buttonList.add(new GuiGJButton(width - 22, 2, 20, 20, "X"));
-
-			textField  = new GuiGJTextField(this.width / 2 - 75, height/2 - 60, 150, 60, "");
-			insertField  = new GuiGJTextField(this.width / 2 - 75, height/2 - 60, 150, 60, "");
-		}
-	}
-	
 	@Override
-	public void drawScreen(int p_73863_1_, int p_73863_2_, float p_73863_3_){
-		textField.drawBackground();
-		textField.drawText();
-		insertField.drawBackground();
-		insertField.drawText();
+	public void initGui(){
+
+		for(int i = 0; i<allowedChars.length();i++){
+			allowedChar[i] = allowedChars.charAt(i);
+			//Debug	Main.log.info(allowedChar[i]);
+		}
+		
+		midX = getMid(width);
+		midY = getMid(height);
+		
+	}
+
+	@Override
+	public void addWidgets(){
+
+		add(insertField = new  GuiGJTextField(midX + 10, midY - 10, 150, 15, "").setAllowedCharacters(allowedChar).setMaxStringLength(255));
+		add(listField = new GuiGJTextField(midX + 10, midY + 50, 150, 120, "").setAllowedCharacters(allowedChar));
+
+		add(new GuiGJButton(midX + 140, midY + 180, 20, 20, "<").setActionCommand("priorPage"));
+		add(new GuiGJButton(midX + 270, midY + 180, 20, 20, ">").setActionCommand("nextPage"));
+		add(new GuiGJButton(midX - 45, midY + 50, 44, 20, "Connect").setActionCommand("connect"));
+		add(new GuiGJButton(midX - 45, midY + 100, 44, 20, "Add").setActionCommand("addLink"));
+		add(new GuiGJButton(midX +350, midY + 100, 44, 20, "Delete").setActionCommand("deleteLink"));
+		add(new GuiGJButton(midX + 344, midY + 50, 55, 20, "Favourite").setActionCommand("setFav"));
+		add(new GuiGJButton(midX * 2 + 50, midY/2 + 30, 20, 20, "X").setActionCommand("quitGui"));
+
+	}
+
+	@Override
+	public void actionPerformed(String ident, Object ... params){
+
+		if(ident.equals("connect")){
+			Main.log.info("Connecting to: " + listFieldText);
+		}else if(ident.equals("priorPage")){
+			if(pageNumber > 0){
+				pageNumber--;
+				Main.log.info("current page: " + pageNumber);
+			}
+		}else if(ident.equals("nextPage")){
+			pageNumber++;
+			Main.log.info("Current page: " + pageNumber);
+		}else if(ident.equals("addLink")){
+			if(insertFieldText != null){
+				listFieldText.add(insertFieldText);
+			} 
+		}else if(ident.equals("deleteLink")){
+		//	TODO make the currSelectedLink variable
+		//	listFieldText.remove(0);
+		}else if(ident.equals("setFav")){
+		//	TODO make the currSelectedLink variable
+		//	favList.add("Test Fav");
+		}else if(ident.equals("quitGui")){
+			mc.displayGuiScreen(null);
+		}
+
 	}
 
 	@Override
 	public boolean doesGuiPauseGame(){
 		return false;
 	}
-	
+
 }
